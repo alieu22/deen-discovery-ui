92f5c4101445efe16e4a04b9f99f3c9c2e8c09bc
alieu22 <alieusma@gmail.com>
2024-01-11 20:48:34 -0800

    refactored the intial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './Components/Navbar'
+import NavBar from './components/Navbar'
 
 function App() {
  return (
