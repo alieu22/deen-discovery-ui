b5558b9613da8c64d0ca678e2fe7ee09b40dc743
alieu22 <alieusma@gmail.com>
2024-01-11 20:37:51 -0800

    initalization of ui project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './Components/Navbar'
+
+function App() {
+ return (
+  <NavBar />
+ )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar() {
+    return (
+    <>
+    <div className="bg-gray-800">
+      <div className="h-16 px-8 flex items-center">
+        <p className="text-white font-bold">Deen Discovery</p>
+      </div>
+    </div>
+  </>
+);
+}
+export default NavBar;
